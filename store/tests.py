84db6fdd8327eafadf3e0a1820476bf1fa8e0fe8
Rui Vilela <tek@qwertytek.com>
2024-08-12 06:30:22 +0100

    add migration product model

--- store/tests.py
+++ store/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from .models import Product
+class ProductModelTest(TestCase):
+    def setUp(self):
+        self.product = Product.objects.create(name="Apple")
 
-# Create your tests here.
+    def test_product_creation(self):
+        self.assertEqual(self.product.name, "Apple")
+
+    def test_product_str_method(self):
+        self.assertEqual(str(self.product), "Apple")
