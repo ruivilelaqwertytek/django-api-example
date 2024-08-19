8b996c199ea092f9b0ba80b9934a7e8c73da87b3
Rui Vilela <tek@qwertytek.com>
2024-08-19 04:33:43 +0100

    first steps of django_frameworks installation, updated requirements.txt

--- config/settings.py
+++ config/settings.py
@@ -37,7 +37,8 @@ INSTALLED_APPS = [
     "django.contrib.sessions",
     "django.contrib.messages",
     "django.contrib.staticfiles",
-    "store"
+    "rest_framework",
+    "store",
 ]
 
 MIDDLEWARE = [
@@ -122,3 +123,9 @@ STATIC_URL = "static/"
 # https://docs.djangoproject.com/en/5.1/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
+
+REST_FRAMEWORK = {
+    'DEFAULT_PERMISSION_CLASSES': [
+        'rest_framework.permissions.DjangoModelPermissionsOrAnonReadOnly'
+    ]
+}
